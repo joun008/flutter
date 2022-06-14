f405e6e87f8970a054dbcd7294ba49289551f2fd
joun008 <jouny007@gm.gist.ac.kr>
2022-06-15 01:22:39 +0900

    Svg, 기본 constant 설정

---
+++ practice/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffe7f7f6);
+const Color bgColor = Colors.white;
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 25.0;
