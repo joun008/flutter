1c5ea7b5115c33414b6ae9da38b8c3083ee3a7e1
joun008 <jouny007@gm.gist.ac.kr>
2022-06-15 05:18:25 +0900

    새로운 color추가

--- practice/lib/constants.dart
+++ practice/lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xffe7f7f6);
 const Color bgColor = Colors.white;
+const Color subColor = Color(0xff2fc0b2);
 
-const double defaultPadding = 16.0;
+const double defaultPadding = 8.0;
 const double defaultBorderRadius = 25.0;
