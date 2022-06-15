3db612bdd85962515ebd88c430548330fedce37d
joun008 <jouny007@gm.gist.ac.kr>
2022-06-15 10:07:33 +0900

    home 페이지에 medal week 구현

--- practice/lib/constants.dart
+++ practice/lib/constants.dart
@@ -4,5 +4,4 @@ const Color primaryColor = Color(0xffe7f7f6);
 const Color bgColor = Colors.white;
 const Color subColor = Color(0xff2fc0b2);
 
-const double defaultPadding = 8.0;
 const double defaultBorderRadius = 25.0;
